cd8dada6cec2747f4cc690b09ca7a8676c9b9389
Kenny Yang <kennyyangh@gmail.com>
2020-09-14 14:47:56 -0700

    finished work

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Jumbotron from './components/Jumbotron'
+import Navbar from './components/Navbar'
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Jumbotron />
+
+    </>
+  );
+}
+
+export default App;
